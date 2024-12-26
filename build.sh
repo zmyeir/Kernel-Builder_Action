86e8841c74ac641051fdb5c6b06fafa753fe07d8
Zmyeir <106296841+zmyeir@users.noreply.github.com>
2024-12-26 20:37:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,41 +3,21 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-#DO NOT GO OVER 4
-#MAX_CONCURRENT_BUILDS=1
+curl -so repo https://storage.googleapis.com/git-repo-downloads/repo
+sudo mv repo /usr/bin
+sudo chmod +x /usr/bin/repo
 
-# Check if 'builds' folder exists, create it if not
-if [ ! -d "./builds" ]; then
-    echo "'builds' folder not found. Creating it..."
-    mkdir -p ./builds
-else
-    echo "'builds' folder already exists removing it."
-    rm -rf ./builds
-    mkdir -p ./builds
-fi
-
-cd ./builds
+export KBUILD_BUILD_USER=buildbot
+export KBUILD_BUILD_HOST=buildhost
 
 # Iterate over configurations
 build_config() {
-    CONFIG=$1
-    CONFIG_DETAILS=${CONFIG}
-
-    # Create a directory named after the current configuration
-    echo "Creating folder for configuration: $CONFIG..."
-    mkdir -p "$CONFIG"
-    cd "$CONFIG"
-    
-    # Split the config details into individual components
-    IFS="-" read -r ANDROID_VERSION KERNEL_VERSION SUB_LEVEL DATE <<< "$CONFIG_DETAILS"
-    
-    # Formatted branch name for each build (e.g., android14-5.15-2024-01)
-    FORMATTED_BRANCH="${ANDROID_VERSION}-${KERNEL_VERSION}-${DATE}"
+    GKI_VERSION=android14-6.1
 
     # Log file for this build in case of failure
-    LOG_FILE="../${CONFIG}_build.log"
+    # LOG_FILE="../${CONFIG}_build.log"
 
-    echo "Starting build for $CONFIG using branch $FORMATTED_BRANCH..."
+    echo "Starting build ${GKI_VERSION}-lts..."
 
     # Check if susfs4ksu repo exists, remove it if it does
     if [ -d "./susfs4ksu" ]; then
@@ -45,24 +25,24 @@ build_config() {
         rm -rf ./susfs4ksu
     fi
     echo "Cloning susfs4ksu repository..."
-    git clone https://gitlab.com/simonpunk/susfs4ksu.git -b "gki-${ANDROID_VERSION}-${KERNEL_VERSION}"
+    git clone https://gitlab.com/simonpunk/susfs4ksu.git -b "gki-${GKI_VERSION}"
 
-    mkdir -p "$CONFIG"
+    mkdir -p "$GKI_VERSION"
     ls -lah .
     pwd
-    cd "$CONFIG"
+    cd "$GKI_VERSION"
     pwd
 
     # Initialize and sync kernel source with updated repo commands
     echo "Initializing and syncing kernel source..."
-    repo init --depth=1 --u https://android.googlesource.com/kernel/manifest -b common-${FORMATTED_BRANCH}
-    REMOTE_BRANCH=$(git ls-remote https://android.googlesource.com/kernel/common ${FORMATTED_BRANCH})
+    repo init --depth=1 --u https://android.googlesource.com/kernel/manifest -b common-${GKI_VERSION}-lts
+    REMOTE_BRANCH=$(git ls-remote https://android.googlesource.com/kernel/common ${GKI_VERSION}-lts)
     DEFAULT_MANIFEST_PATH=.repo/manifests/default.xml
     
     # Check if the branch is deprecated and adjust the manifest
     if grep -q deprecated <<< $REMOTE_BRANCH; then
-        echo "Found deprecated branch: $FORMATTED_BRANCH"
-        sed -i "s/\"${FORMATTED_BRANCH}\"/\"deprecated\/${FORMATTED_BRANCH}\"/g" $DEFAULT_MANIFEST_PATH
+        echo "Found deprecated branch: ${GKI_VERSION}-lts"
+        sed -i "s/\"${GKI_VERSION}-lts\"/\"deprecated\/${GKI_VERSION}-lts\"/g" $DEFAULT_MANIFEST_PATH
     fi
 
     # Verify repo version and sync
@@ -75,7 +55,7 @@ build_config() {
 
     echo "Applying SUSFS patches..."
     cp ../susfs4ksu/kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch ./KernelSU/
-    cp ../susfs4ksu/kernel_patches/50_add_susfs_in_gki-android14-6.1.patch ./common/
+    cp ../susfs4ksu/kernel_patches/50_add_susfs_in_gki-${GKI_VERSION}.patch ./common/
     cp ../susfs4ksu/kernel_patches/fs/susfs.c ./common/fs/
     cp ../susfs4ksu/kernel_patches/include/linux/susfs.h ./common/include/linux/
     cp ../susfs4ksu/kernel_patches/fs/sus_su.c ./common/fs/
@@ -83,14 +63,11 @@ build_config() {
 
     # Apply the patches
     cd ./KernelSU
+    pwd
     patch -p1 < 10_enable_susfs_for_ksu.patch
     cd ../common
-    patch -p1 < 50_add_susfs_in_gki-${ANDROID_VERSION}-${KERNEL_VERSION}.patch
-    # Add lxc support 
-    git clone https://github.com/tomxi1997/lxc-docker-support-for-android
-    ls -lah .
-    echo 'source "lxc-docker-support-for-android/Kconfig"' >> "Kconfig"
-    sh $(pwd)/lxc-docker-support-for-android/runcpatch.sh $(pwd)/kernel/cgroup/cgroup.c
+    pwd
+    patch -p1 < 50_add_susfs_in_gki-${GKI_VERSION}.patch
     cd ..
 
     # Add configuration settings for SUSFS
@@ -108,26 +85,22 @@ build_config() {
     echo "CONFIG_KSU_SUSFS_SUS_SU=y" >> ./common/arch/arm64/configs/gki_defconfig
 
 
-
     # Build kernel
-    echo "Building kernel for $CONFIG..."
-        # Use Bazel build if build.sh exists
+    echo "Building kernel..."
     echo "Running Bazel build..."
-    sed -i "/stable_scmversion_cmd/s/-maybe-dirty/-Ryhoaca+/g" ./build/kernel/kleaf/impl/stamp.bzl
+    sed -i "/stable_scmversion_cmd/s/android14.*-maybe-dirty/lts-Ryhoaca+/g" ./build/kernel/kleaf/impl/stamp.bzl
     sed -i '2s/check_defconfig//' ./common/build.config.gki
     rm -rf ./common/android/abi_gki_protected_exports_aarch64
     rm -rf ./common/android/abi_gki_protected_exports_x86_64
     tools/bazel build --config=fast //common:kernel_aarch64_dist
     # Creating Boot imgs
-    echo "Creating boot.imgs..."
-    if [ "$ANDROID_VERSION" = "android14" ]; then
-        cp ./bazel-bin/common/kernel_aarch64/Image /mnt/kernel_workspace/
-    fi
+    echo "Creating Image..."
+    cp ./bazel-bin/common/kernel_aarch64/Image /mnt/kernel_workspace/
 
 }
 
 # Concurrent build management
-build_config "android14-6.1-X-lts"
+build_config
 
 
 echo "Build process complete."
