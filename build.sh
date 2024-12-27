ecdc58e75baf7c9e9893462481339c9cbf5c2b93
Zmyeir <106296841+zmyeir@users.noreply.github.com>
2024-12-27 08:56:10 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -101,6 +101,7 @@ build_config() {
     echo "Building kernel..."
     echo "Running Bazel build..."
     sed -i "/stable_scmversion_cmd/s/maybe-dirty/Ryhoaca/g" ./build/kernel/kleaf/impl/stamp.bzl
+    sed -i 's/-$android_release-$kmi_generation/-lts/;s/$android_release/-lts/' ./common/scripts/setlocalversion
     sed -i '2s/check_defconfig//' ./common/build.config.gki
     rm -rf ./common/android/abi_gki_protected_exports_aarch64
     rm -rf ./common/android/abi_gki_protected_exports_x86_64
