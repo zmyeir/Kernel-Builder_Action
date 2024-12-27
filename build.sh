a7beb1bb35627f561f2ca6512077d53e5b896659
Zmyeir <106296841+zmyeir@users.noreply.github.com>
2024-12-27 09:50:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -87,6 +87,7 @@ build_config() {
 
     # Add config
     echo "CONFIG_PID_NS=y" >> ${KWORKSPACE}/${GKI_VERSION}/common/arch/arm64/configs/gki_defconfig
+    echo "CONFIG_POSIX_MQUEUE=y" >> ${KWORKSPACE}/${GKI_VERSION}/common/arch/arm64/configs/gki_defconfig
     echo "CONFIG_IPC_NS=y" >> ${KWORKSPACE}/${GKI_VERSION}/common/arch/arm64/configs/gki_defconfig
     echo "CONFIG_USER_NS=y" >> ${KWORKSPACE}/${GKI_VERSION}/common/arch/arm64/configs/gki_defconfig
     #echo "CONFIG_CGROUP_DEVICE=y" >> ${KWORKSPACE}/${GKI_VERSION}/common/arch/arm64/configs/gki_defconfig
@@ -94,8 +95,8 @@ build_config() {
     #echo "CONFIG_BRIDGE_NETFILTER=y" >> ${KWORKSPACE}/${GKI_VERSION}/common/arch/arm64/configs/gki_defconfig
     #echo "CONFIG_NETFILTER_XT_MATCH_ADDRTYPE=y" >> ${KWORKSPACE}/${GKI_VERSION}/common/arch/arm64/configs/gki_defconfig
     #echo "CONFIG_NETFILTER_XT_MATCH_IPVS=y" >> ${KWORKSPACE}/${GKI_VERSION}/common/arch/arm64/configs/gki_defconfig
-    echo "CONFIG_POSIX_MQUEUE=y" >> ${KWORKSPACE}/${GKI_VERSION}/common/arch/arm64/configs/gki_defconfig
     #echo "CONFIG_CGROUP_PIDS=y" >> ${KWORKSPACE}/${GKI_VERSION}/common/arch/arm64/configs/gki_defconfig
+    cat ${KWORKSPACE}/${GKI_VERSION}/common/arch/arm64/configs/gki_defconfig
 
     # Build kernel
     echo "Building kernel..."
